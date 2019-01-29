1c038428924f7236ff850fff2829ff43485c3a40
Michael Ventura <michael.ventura@gmail.com>
2019-01-29 14:24:15 -0500

    fix task deps so that lutra actually runs on compile

--- plugin/src/main/groovy/net/inetalliance/lutra/LutraPlugin.groovy
+++ plugin/src/main/groovy/net/inetalliance/lutra/LutraPlugin.groovy
@@ -2,23 +2,33 @@ package net.inetalliance.lutra
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.artifacts.Configuration
 
+@SuppressWarnings("unused")
 class LutraPlugin implements Plugin<Project> {
   @Override
   void apply(Project project) {
     project.apply(plugin: 'java')
     LutraExtension ext = project.extensions.create('lutraExt', LutraExtension.class);
-    Configuration lutraConfiguration = project.configurations.maybeCreate('lutra')
-    project.task('lutra', type: LutraTask, group: 'lutra', description: 'Generate lutra source from html files')
-    project.task('deleteGeneratedSources', type: CleanTask, group: 'lutra', description: 'Delete java source code ' +
-        'generated from html files.')
+    project.configurations.maybeCreate('lutra')
+
+     def lutra = project.task('lutra',
+        type: LutraTask,
+        group: 'lutra',
+        description: 'Generate lutra source from html files')
+
+     def clean = project.task('deleteGeneratedSources',
+        type: CleanTask,
+        group: 'lutra',
+        description: 'Delete java source code generated from html files.')
+
+    project.tasks.compileJava.dependsOn lutra
+    project.tasks.compileTestJava.dependsOn lutra
+
     project.afterEvaluate {
-        project.sourceSets.main.java.srcDirs += project.lutra.outputDirectory
-        project.compileJava.dependsOn project.lutra
+      project.sourceSets.main.java.srcDirs += project.lutra.outputDirectory
     }
-    if(ext.deleteGeneratedSourcesOnClean) {
-      project.clean.dependsOn project.deleteGeneratedSources
+    if (ext.deleteGeneratedSourcesOnClean) {
+      project.tasks.clean.dependsOn clean
     }
   }
 }
--- plugin/src/main/groovy/net/inetalliance/lutra/LutraTask.groovy
+++ plugin/src/main/groovy/net/inetalliance/lutra/LutraTask.groovy
@@ -1,11 +1,7 @@
 package net.inetalliance.lutra
 
-import net.inetalliance.lutra.elements.Attribute
 import net.inetalliance.lutra.elements.Element
-import net.inetalliance.lutra.elements.HtmlElement
-import net.inetalliance.lutra.elements.TextContent
 import net.inetalliance.lutra.rules.ValidationErrors
-import net.inetalliance.lutra.util.Escaper
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.*
 import org.xml.sax.SAXException
@@ -20,9 +16,9 @@ class LutraTask extends DefaultTask {
   @Input
   String exclude = ''
   @OutputDirectory
-  File outputDirectory = new File('src/lutra/java')
+  File outputDirectory = new File(project.projectDir, 'src/lutra/java')
   @InputDirectory
-  File basedir = new File(".")
+  File inputDirectory = new File(project.projectDir, 'src/main/webapp')
   @Input
   String packageName
 
@@ -32,8 +28,60 @@ class LutraTask extends DefaultTask {
   @SuppressWarnings("unused")
   @TaskAction
   def generate() {
-    getLogger().debug("basedir is %s", basedir.getAbsolutePath())
-    execute("src/main/webapp")
+    def log = getLogger()
+    log.info("Generating lazy-parsed classes for html files in  ${inputDirectory.getPath()}")
+
+    if (!outputDirectory.exists())
+      if (!outputDirectory.mkdirs())
+        log.error("Could not make output dir!")
+
+    String packageName = "${packageName}.lutra"
+    log.info("Package will be ${packageName}")
+
+    final Collection<String> parseErrors = new ArrayList<>(1024)
+    final Collection<String> validationErrors = new ArrayList<>(1024)
+
+    Predicate<? super File> exclude
+    if (this.exclude == null || this.exclude.isEmpty()) {
+      exclude = { File f -> true }
+    } else {
+      def p = Pattern.compile(this.exclude)
+      exclude = { File f -> !p.matcher(f.getAbsolutePath()).matches() }
+    }
+
+    Files.walk(inputDirectory.toPath())
+        .map({ p -> p.toFile() })
+        .filter(exclude)
+        .filter({ f -> f.getName().endsWith(".html") })
+        .each({ genInput ->
+      try {
+        final String filePath = genInput.getAbsolutePath().replace(inputDirectory.getAbsolutePath(), "")
+        final int lastSlash = filePath.lastIndexOf('/')
+        final String withoutFilename = lastSlash < 0 ? "" : filePath.substring(0, lastSlash)
+        final String genPackage = String.format("%s%s", packageName, withoutFilename.replace('/', '.'))
+        if (log.isDebugEnabled()) {
+          log.debug(String.format("relative path: %s", filePath))
+          log.debug(String.format("package: %s", genPackage))
+          log.debug(String.format("without file: %s", withoutFilename))
+        }
+        generate(genPackage, genInput, filePath, parseErrors, validationErrors)
+      }
+      catch (SAXException e) {
+        log.error(String.format("Error while processing %s:\n%s", genInput.getAbsolutePath(), e.getMessage()))
+      }
+      catch (Exception e) {
+        log.error("unknown problem", e)
+      }
+    })
+
+
+    if (validationErrors.size() + parseErrors.size() > 0) {
+      for (final String error : validationErrors)
+        log.error(error)
+      for (final String error : parseErrors)
+        log.error(error)
+      throw new TaskExecutionException(this, new IllegalArgumentException("source errors"))
+    }
 
 
   }
@@ -82,80 +130,16 @@ class LutraTask extends DefaultTask {
     return buffer.toString()
   }
 
-  def execute(String path, boolean preparsed = false) {
-    def root = new File(basedir, path)
-    if (!root.exists()) {
-      getLogger().info("no web root found")
-      return
-    }
-    def log = getLogger()
-    log.info("Generating ${preparsed ? 'pre-parsed' : 'lazy-parsed'} classes for html files in  ${root.getAbsolutePath()}")
-
-    if (!outputDirectory.exists())
-      if (!outputDirectory.mkdirs())
-        log.error("Could not make output dir!")
-
-    String packageName = "${packageName}.lutra"
-    if (preparsed)
-      packageName += ".preparsed"
-    log.info("Package will be ${packageName}")
-
-    final Collection<String> parseErrors = new ArrayList<>(1024)
-    final Collection<String> validationErrors = new ArrayList<>(1024)
-
-    Predicate<? super File> exclude
-    if (preparsed || this.exclude == null || this.exclude.isEmpty()) {
-      exclude = { File f -> true }
-    } else {
-      def p = Pattern.compile(this.exclude)
-      exclude = { File f -> !p.matcher(f.getAbsolutePath()).matches() }
-    }
-
-    Files.walk((root.toPath()))
-        .map({ p -> p.toFile() })
-        .filter(exclude)
-        .filter({ f -> f.getName().endsWith(".html") })
-        .each({ genInput ->
-      try {
-        final String filePath = genInput.getAbsolutePath().replace(root.getAbsolutePath(), "")
-        final int lastSlash = filePath.lastIndexOf('/')
-        final String withoutFilename = lastSlash < 0 ? "" : filePath.substring(0, lastSlash)
-        final String genPackage = String.format("%s%s", packageName, withoutFilename.replace('/', '.'))
-        if (log.isDebugEnabled()) {
-          log.debug(String.format("relative path: %s", filePath))
-          log.debug(String.format("package: %s", genPackage))
-          log.debug(String.format("without file: %s", withoutFilename))
-        }
-        generate(genPackage, genInput, filePath, parseErrors, validationErrors, preparsed)
-      }
-      catch (SAXException e) {
-        log.error(String.format("Error while processing %s:\n%s", genInput.getAbsolutePath(), e.getMessage()))
-      }
-      catch (Exception e) {
-        log.error("unknown problem", e)
-      }
-    })
-
-
-    if (validationErrors.size() + parseErrors.size() > 0) {
-      for (final String error : validationErrors)
-        log.error(error)
-      for (final String error : parseErrors)
-        log.error(error)
-      throw new TaskExecutionException(this, new IllegalArgumentException("source errors"))
-    }
-  }
 
   void generate(final String packageName, final File input, final String filePath,
-                final Collection<String> parseErrors, final Collection<String> validationErrors,
-                final boolean preparsed)
+                final Collection<String> parseErrors, final Collection<String> validationErrors)
       throws IOException, SAXException {
-    generate(outputDirectory, packageName, input, filePath, parseErrors, validationErrors, preparsed)
+    generate(outputDirectory, packageName, input, filePath, parseErrors, validationErrors)
   }
 
   File generate(final File outputDirectory, final String packageName, final File input,
                 final String filePath, final Collection<String> parseErrors,
-                final Collection<String> validationErrors, final boolean preparsed)
+                final Collection<String> validationErrors)
       throws IOException, SAXException {
     def log = getLogger()
     final File outputDir = makeOutputDir(outputDirectory, packageName)
@@ -172,7 +156,7 @@ class LutraTask extends DefaultTask {
         log.debug(String.format("%s is not modified", file.getAbsolutePath()))
       return file
     } else
-      log.info(String.format("Generating %s API for %s", preparsed ? "pre-parsed" : "lazy-parsed", input.getAbsolutePath()))
+      log.info(String.format("Generating lazy-parsed API for %s", input.getAbsolutePath()))
     if (!file.getParentFile().exists() && !file.getParentFile().mkdirs())
       log.error(String.format("Could not make parent dir %s", file.getParentFile().getAbsolutePath()))
 
@@ -200,83 +184,45 @@ class LutraTask extends DefaultTask {
         final Map<String, Element> byId = builder.getById()
 
         out.append("package ").append(packageName).append(";\n\n")
-        if (preparsed) {
-          final LazyDocument document = builder.buildGeneric()
-          final Set<Class> imports = new TreeSet<>({ a, b -> a.name.compareTo(b.name) })
-          final StringBuilder cls = new StringBuilder(1024)
-          imports.add(HtmlElement.class)
-          imports.add(Attribute.class)
-
-          cls.append("\n/**\n")
-          cls.append(" * Generated from ").append(input.getAbsolutePath()).append(" on ")
-          cls.append(DATE_FORMAT.format(new Date())).append(".\n")
-          cls.append(" *\n")
-          cls.append(" * @version ").append(DATE_FORMAT.format(new Date(input.lastModified()))).append("\n")
-          cls.append(" */\n")
-          cls.append("public class ").append(name).
-              append(" extends ").append(HtmlElement.class.getSimpleName()).
-              append("\n{\n")
-          cls.append("\tpublic final HeadElement head;\n")
-          cls.append("\tpublic final BodyElement body;\n\n")
-          cls.append("\tpublic ").append(name).append("()\n").
-              append("\t{")
-          cls.append("\n\t\thead = ")
-          generatePreparsed(cls, imports, document.head, "\t\t\t")
-          cls.append(";\n\t\tbody = ")
-          generatePreparsed(cls, imports, document.body, "\t\t\t")
-          cls.append(";\n\t\taddChild(head, body);")
-          for (final Map.Entry<Attribute, String> entry : document.root.getAttributes().entrySet()) {
-            cls.append("\n\t\tsetAttribute(Attribute.")
-            cls.append(entry.getKey().name()).append(", \"")
-            cls.append(Escaper.javaString.escape(entry.getValue())).append("\");")
+        addImport(out, LazyDocument.class)
+        addImport(out, DocumentBuilder.class)
+        addImport(out, Element.class)
+        addImport(out, ElementBundle.class)
+        addImport(out, EnumMap.class)
+        addImport(out, Map.class)
+        final Set<Class> elementTypes = new HashSet<>(byId.size())
+        for (final Element element : byId.values()) {
+          if (!elementTypes.contains(element.getClass())) {
+            elementTypes.add(element.getClass())
+            addImport(out, element.getClass())
           }
-          cls.append("\n\t}")
-          cls.append("\n}")
-
-          for (final Class imported : imports)
-            addImport(out, imported)
-          out.append(cls)
-        } else {
-          addImport(out, LazyDocument.class)
-          addImport(out, DocumentBuilder.class)
-          addImport(out, Element.class)
-          addImport(out, ElementBundle.class)
-          addImport(out, EnumMap.class)
-          addImport(out, Map.class)
-          final Set<Class> elementTypes = new HashSet<>(byId.size())
-          for (final Element element : byId.values()) {
-            if (!elementTypes.contains(element.getClass())) {
-              elementTypes.add(element.getClass())
-              addImport(out, element.getClass())
-            }
-          }
-
-          out.append("\n/**\n")
-          out.append(" * Generated from ").append(input.getAbsolutePath()).append(" on ")
-          out.append(DATE_FORMAT.format(new Date())).append(".\n")
-          out.append(" *\n")
-          out.append(" * @version ").append(DATE_FORMAT.format(new Date(input.lastModified()))).append("\n")
-          out.append(" */\n")
-          out.append("public class ").append(name).
-              append(" extends ").append(LazyDocument.class.getSimpleName()).
-              append("\n{\n")
-
-          out.append("\t/** An object holding all the elements in this document */\n")
-          out.append("\tpublic final Tags tags;\n")
-          out.append("\t/** A map holding all the elements in this document */\n")
-          out.append("\tprivate final Map<Id,Element> map;\n")
-          out.append("\n\tpublic static final String ").append(LazyDocument.FILE_PATH_FIELD).append(" = \"").append(
-              filePath).append("\";\n")
-
-          out.append("\n\tpublic ").append(name).append("(final ").append(DocumentBuilder.class.getSimpleName()).append(
-              " builder)\n\t{\n")
-          out.append("\t\tsuper(builder);\n")
-          out.append("\t\tmap = new EnumMap<>(Id.class);\n")
-          out.append("\t\ttags = new Tags(this, map);\n")
-          out.append("\t}\n\n")
-          generateTagsClass(out, byId, name)
-          out.append("}")
         }
+
+        out.append("\n/**\n")
+        out.append(" * Generated from ").append(input.getAbsolutePath()).append(" on ")
+        out.append(DATE_FORMAT.format(new Date())).append(".\n")
+        out.append(" *\n")
+        out.append(" * @version ").append(DATE_FORMAT.format(new Date(input.lastModified()))).append("\n")
+        out.append(" */\n")
+        out.append("public class ").append(name).
+            append(" extends ").append(LazyDocument.class.getSimpleName()).
+            append("\n{\n")
+
+        out.append("\t/** An object holding all the elements in this document */\n")
+        out.append("\tpublic final Tags tags;\n")
+        out.append("\t/** A map holding all the elements in this document */\n")
+        out.append("\tprivate final Map<Id,Element> map;\n")
+        out.append("\n\tpublic static final String ").append(LazyDocument.FILE_PATH_FIELD).append(" = \"").append(
+            filePath).append("\";\n")
+
+        out.append("\n\tpublic ").append(name).append("(final ").append(DocumentBuilder.class.getSimpleName()).append(
+            " builder)\n\t{\n")
+        out.append("\t\tsuper(builder);\n")
+        out.append("\t\tmap = new EnumMap<>(Id.class);\n")
+        out.append("\t\ttags = new Tags(this, map);\n")
+        out.append("\t}\n\n")
+        generateTagsClass(out, byId, name)
+        out.append("}")
         return file
       }
       catch (IOException ignored) {
@@ -295,35 +241,6 @@ class LutraTask extends DefaultTask {
     return null
   }
 
-  private static void generatePreparsed(final Appendable cls, final Set<Class> imports, final Element element, final String indent) throws IOException {
-    imports.add(element.getClass())
-    if (element instanceof TextContent)
-      cls.append("new TextContent(\"").append(Escaper.javaString.escape(element.getText())).append("\")")
-    else {
-      if (!element.getAttributes().isEmpty())
-        cls.append('(').append(element.getClass().getSimpleName()).append(") ")
-      cls.append("new ").append(element.getClass().getSimpleName()).append('(')
-      final List<Element> children = element.getChildren()
-      if (children.size() == 1 && children.get(0) instanceof TextContent) {
-        cls.append('"').append(Escaper.javaString.escape(children.get(0).getText())).append('"')
-      } else {
-        if (children.size() > 1)
-          cls.append('\n').append(indent)
-        for (Iterator<Element> iterator = children.iterator(); iterator.hasNext();) {
-          final Element child = iterator.next()
-          generatePreparsed(cls, imports, child, indent + "\t")
-          if (iterator.hasNext())
-            cls.append(",\n").append(indent)
-        }
-      }
-      cls.append(')')
-      for (final Map.Entry<Attribute, String> entry : element.getAttributes().entrySet()) {
-        cls.append('\n').append(indent).append('\t')
-        cls.append(".setAttribute(Attribute.").append(entry.getKey().name())
-        cls.append(", \"").append(entry.getValue()).append("\")")
-      }
-    }
-  }
   private static final Pattern dotPattern = Pattern.compile("\\.")
   private static final Pattern invalidCharactersPattern = Pattern.compile("[.\\-_:]")
 
