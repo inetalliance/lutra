c325ceccf8e14adc4513294b8a1abc2acfdeeb2e
Michael Ventura <michael.ventura@gmail.com>
2019-01-29 14:32:29 -0500

    prepare to publish to plugin repo

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,22 +1,29 @@
 group = "net.inetalliance.lutra"
 plugins {
-    groovy
-    idea
-    `java-gradle-plugin`
+  groovy
+  idea
+  `java-gradle-plugin`
+  id("com.gradle.plugin-publish") version "0.10.0"
 }
 
 dependencies {
-    compile(project(":api"))
-    testCompile("junit:junit:4.8.2")
+  compile(project(":api"))
+  testCompile("junit:junit:4.8.2")
+}
+pluginBundle {
+  website = "https://github.com/inetalliance" 
+  vcsUrl = "https://github.com/inetalliance/lutra" 
+  tags = listOf("xhtml", "java") 
 }
 
 gradlePlugin {
-    plugins {
-        create("simplePlugin") {
-            id = "net.inetalliance.lutra"
-            implementationClass = "net.inetalliance.lutra.LutraPlugin"
-        }
+  plugins {
+    create("lutraPlugin") {
+      id = "net.inetalliance.lutra"
+      displayName = "Lutra XHTML to Java Generator"
+      implementationClass = "net.inetalliance.lutra.LutraPlugin"
     }
+  }
 }
 
 
